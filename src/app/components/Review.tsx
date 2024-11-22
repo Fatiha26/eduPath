e70a03bb23ea18e81e0992dc1b974172028bbed1
Fatiha26 <f6lamia@gmail.com>
2024-11-22 18:18:35 +0600

    code build

--- src/app/components/Review.tsx
+++ src/app/components/Review.tsx
@@ -1,33 +1,50 @@
-"use client"
+"use client";
 
-import Image from 'next/image'
-import React from 'react'
-import { reviewData } from '../../utils/helper/dummy-data/review'
-import Carousel from "react-elastic-carousel";
+import Image from "next/image";
+import React from "react";
+import ReactElasticCarousel from "react-elastic-carousel";
+import { reviewData } from "../../utils/helper/dummy-data/review";
 import styles from "../../../styles/review.module.scss";
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
   { width: 768, itemsToShow: 3 },
-  { width: 1024, itemsToShow: 4 }
+  { width: 1024, itemsToShow: 4 },
 ];
-
 
 const Review = () => {
   return (
     <div className={styles.review}>
-      <h2 className={styles.heading}>Learners love EduPath. See why <br /> they rate us 4.9 out of 5</h2>
-      <Carousel breakPoints={breakPoints} showArrows={false} pagination={true} enableAutoPlay={true} autoPlaySpeed={3000}>
+      <h2 className={styles.heading}>
+        Learners love EduPath. See why <br /> they rate us 4.9 out of 5
+      </h2>
+      <ReactElasticCarousel
+        breakPoints={breakPoints}
+        pagination={true}
+        enableAutoPlay={true}
+        autoPlaySpeed={3000}
+        showArrows={false}
+      >
         {reviewData.map((item, index) => (
           <div key={index} className={styles.review__card}>
             <div className={styles.review__card__top}>
-              <div>
-                <Image src={item.image1} width={56} height={56} alt='pp'
-                  style={{ borderRadius: '50%', width: '56px', height: '56px' }} />
+              <div className={styles.review__card__profile}>
+                <Image
+                  src={item.image1}
+                  width={56}
+                  height={56}
+                  alt="Profile Picture"
+                  style={{ borderRadius: "50%" }}
+                />
                 <p className={styles.review__card__name}>{item.name}</p>
               </div>
               <div>
-                <Image src={item.image2} width={26} height={19} alt='pp' />
+                <Image
+                  src={item.image2}
+                  width={26}
+                  height={19}
+                  alt="Rating Icon"
+                />
               </div>
             </div>
             <div className={styles.review__card__text}>
@@ -35,9 +52,9 @@ const Review = () => {
             </div>
           </div>
         ))}
-      </Carousel>
+      </ReactElasticCarousel>
     </div>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
