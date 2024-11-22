c03705250070ea72d14416850bca7897f7c35f6f
Fatiha26 <f6lamia@gmail.com>
2024-11-22 18:30:28 +0600

    code build try

--- src/app/components/Courses.tsx
+++ src/app/components/Courses.tsx
@@ -1,32 +1,36 @@
 "use client"
-
-import React from 'react';
-import { courseData } from '../../utils/helper/dummy-data/courses';
-import Image from 'next/image';
+import React from "react";
+import { courseData } from "../../utils/helper/dummy-data/courses";
+import Image from "next/image";
 import styles from "../../../styles/course.module.scss";
 import { MdTimer } from "react-icons/md";
 import Carousel from "react-elastic-carousel";
 import { FaBookOpen } from "react-icons/fa6";
 import { FiShoppingCart } from "react-icons/fi";
 import { FaRegHeart } from "react-icons/fa";
+
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
   { width: 768, itemsToShow: 2 },
-  { width: 1024, itemsToShow: 3 }
+  { width: 1024, itemsToShow: 3 },
 ];
-
 
 const ElasticCarousel = () => {
   return (
     <div className={styles.courses__container}>
-      <h2 >Our Popular Courses</h2>
+      <h2>Our Popular Courses</h2>
       <p>
-        By taking proactive steps to nurture mental health, we can enhance our quality of life,
-        <br />
+        By taking proactive steps to nurture mental health, we can enhance our
+        quality of life, <br />
         build resilience, and foster a sense of inner peace and balance.
       </p>
       <div className={styles.course__slider}>
-        <Carousel breakPoints={breakPoints} pagination={false} enableAutoPlay={true} autoPlaySpeed={3000}>
+        <Carousel
+          breakPoints={breakPoints}
+          pagination={false}
+          enableAutoPlay={true}
+          autoPlaySpeed={3000}
+        >
           {courseData.map((item, index) => (
             <div key={index} className={styles.course__card}>
               <div className={styles.image__div}>
@@ -37,8 +41,12 @@ const ElasticCarousel = () => {
                 </div>
               </div>
               <div className={styles.course__link}>
-                <a href="#" className={styles.course__link1}>{item.link1}</a>
-                <a href="#" className={styles.course__link2}>{item.link2}</a>
+                <a href="#" className={styles.course__link1}>
+                  {item.link1}
+                </a>
+                <a href="#" className={styles.course__link2}>
+                  {item.link2}
+                </a>
               </div>
               <h3>{item.heading}</h3>
               <div className={styles.course__meta}>
@@ -47,17 +55,23 @@ const ElasticCarousel = () => {
               </div>
               <div className={styles.course__details}>
                 <div className={styles.course__details__inner}>
-                  <MdTimer size={24} color='#9998E1' /><span>{item.time}</span>
+                  <MdTimer size={24} color="#9998E1" />
+                  <span>{item.time}</span>
                 </div>
                 <span className={styles.course__details__line}>|</span>
                 <div className={styles.course__details__inner}>
-                  <FaBookOpen size={24} color='#9998E1' /><span>{item.lession}</span>
+                  <FaBookOpen size={24} color="#9998E1" />
+                  <span>{item.lession}</span>
                 </div>
               </div>
               <div className={styles.course__footer}>
                 <div className={styles.instructor__info}>
-                  <Image src={item.img2} alt="Instructor" width={40} height={40}
-                    style={{ borderRadius: '50%', width: '40px', height: '40px' }}
+                  <Image
+                    src={item.img2}
+                    alt="Instructor"
+                    width={40}
+                    height={40}
+                    style={{ borderRadius: "50%", width: "40px", height: "40px" }}
                   />
                   <p>{item.name}</p>
                 </div>
